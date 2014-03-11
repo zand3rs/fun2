7ad320a852f48fba1e1ce6ec2d5232417e23c572
zander <zand3rs@gmail.com>
2014-03-11 15:02:20 +0800

    ftp fetcher script

---
+++ scripts/fun2_default_unli.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+FTP_HOST="localhost"
+FTP_USER="zander"
+FTP_PASS="zander"
+
+REMOTE_DIR="ftp"
+LOCAL_DIR="local"
+
+BASE_DIR="${0%/*}"
+CURRENT_FILE="$(date '+%Y_%m_%d_%H')*"
+
+/usr/bin/expect -f - <<EOFEXPECT
+    set timeout 60
+    spawn sftp $FTP_USER@$FTP_HOST
+    expect "password:"
+    send "$FTP_PASS\r";
+    expect "sftp>"
+    send "lcd $BASE_DIR\r";
+    expect "sftp>"
+    send "lcd $LOCAL_DIR\r";
+    expect "sftp>"
+    send "cd $REMOTE_DIR\r";
+    expect "sftp>"
+    send "mget $CURRENT_FILE\r";
+    expect "sftp>"
+    send "bye\r";
+    expect eof
+EOFEXPECT
