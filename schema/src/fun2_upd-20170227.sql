f405d583f32e32b05d3e24edf2f2282c242c0768
zander <zand3rs@gmail.com>
2017-02-08 16:51:08 +0800

    script for adding  brand column

---
+++ schema/src/fun2_upd-20170227.sql
@@ -0,0 +1,3 @@
+alter table services add BRAND VARCHAR2(16) default 'PREPAID';
+alter table request_log add BRAND VARCHAR2(16) default 'PREPAID';
+alter table keyword_msg add BRAND VARCHAR2(16) default 'PREPAID';
