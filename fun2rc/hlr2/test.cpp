1db2494939b3c9adb46a9341a4ced1241a77d81a
zander <zand3rs@gmail.com>
2017-02-09 12:25:43 +0800

    updated parsing test

--- fun2rc/hlr2/test.cpp
+++ fun2rc/hlr2/test.cpp
@@ -2,24 +2,30 @@
 #include "libfuc.h"
 
 int main(int argc, char* argv[]) {
-    HttpClient hc;
-    int http_code = hc.httpGet("http://urlecho.appspot.com/echo", 10);
+    //HttpClient hc;
+    //int http_code = hc.httpGet("http://urlecho.appspot.com/echo", 10);
 
-    printf("response:\n status: %d\n headers: %s\n body: %s\n",
-            http_code, hc.getResponseHeaders(), hc.getResponseBody());
+    //printf("response:\n status: %d\n headers: %s\n body: %s\n",
+    //        http_code, hc.getResponseHeaders(), hc.getResponseBody());
 
-    char buf[512];
-    snprintf(buf, sizeof(buf), "%s", hc.getResponseHeaders());
+    //char buf[512];
+    //snprintf(buf, sizeof(buf), "%s", hc.getResponseHeaders());
 
     char* token;
     char* pbuf;
 
     //pbuf = strstr(buf, "Content-Type");
-    pbuf = strstr(buf, "Cache-Control");
+    char buf[512] = "HTTP/1.1 307 Temporary Redirect\n"
+        "Location: http://10.157.26.69:8001/00229898\n"
+        "Server: Huawei web server\n"
+        "Content-Type: text/xml; charset=\"utf-8\"\n"
+        "Content-Length: 368\n";
+
+    pbuf = strstr(buf, "Location");
     if (pbuf) {
         token = strtok_r(NULL, ":\r\n", &pbuf);
         printf("token: [%s]\n", token);
-        token = strtok_r(NULL, ":\r\n", &pbuf);
+        token = strtok_r(NULL, "\r\n", &pbuf);
         printf("token: [%s]\n", token);
     }
 
